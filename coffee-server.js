a4d86ab1657711df283f1c2f27c3b5614927ec9b
Karl Pokus <karl.pokus@gmail.com>
2016-10-22 15:26:58 +0200

    latest version :D

--- coffee-server.js
+++ coffee-server.js
@@ -1,6 +1,7 @@
 http = require('http')
 fs = require('fs')
 path = require('path')
+cal = require('./calendar.coffee')
 server = http.createServer()
 port = process.env.PORT or 3000
 
@@ -16,6 +17,11 @@ server.on('request', (req, res) ->
     # img
   else if /img/.test(req.url)
     send req.url, res
+    # post
+  else if req.method == 'POST'
+    console.log 'POST!'
+    cal.create()
+    res.end 'Event created'
   return
 ).listen port, ->
   console.log 'Server running..'
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http'),
     fs = require('fs'),
     path = require('path'),
+    cal = require('./calendar.coffee'),
     server = http.createServer(),
     port = process.env.PORT || 3000,
     send = function(p, res){
@@ -11,12 +12,20 @@ var http = require('http'),
 
 server
   .on('request', function(req, res){
+
     // index.html
     if (req.method === 'GET' && req.url === '/') {
       send('index.html', res);
+
       // img
     } else if (/img/.test(req.url)) {
       send(req.url, res);
+
+      // post
+    } else if (req.method === 'POST') {
+      console.log('POST!');
+      cal.create();
+      res.end('Event created');
     }
   }).listen(port, function(){
     console.log('Server running..');
